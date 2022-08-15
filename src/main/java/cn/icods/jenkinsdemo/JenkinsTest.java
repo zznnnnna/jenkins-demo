d1a23baae78234ca891201c067d74f67a75cf587
zznnnnna <rex.zhao@cloudwise.com>
2022-08-15 15:58:55 +0800

    jenkins

--- src/main/java/cn/icods/jenkinsdemo/JenkinsTest.java
+++ src/main/java/cn/icods/jenkinsdemo/JenkinsTest.java
@@ -1,8 +1,20 @@
-package cn.icods.jenkinsdemo;/**
-   * @ClassName JenkinsTest 
-   * @Description TODO
-   * @Author rex.zhao
-   * @Date 2022/8/15 3:56 PM
-   * @Version 1.0
-   */ public class JenkinsTest {
+package cn.icods.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName JenkinsTest
+ * @Description TODO
+ * @Author rex.zhao
+ * @Date 2022/8/15 3:56 PM
+ * @Version 1.0
+ */
+@RestController
+public class JenkinsTest {
+    @GetMapping("/jenkins")
+    public String hello() {
+        return "hello jenkins!!!";
+
+    }
 }
