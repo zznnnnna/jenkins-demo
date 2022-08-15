8ffe45ddf49a172d4677afd13ec46f514160ec81
zznnnnna <rex.zhao@cloudwise.com>
2022-08-15 15:58:09 +0800

    first commit

---
+++ src/main/java/cn/icods/jenkinsdemo/JenkinsTest.java
@@ -0,0 +1,8 @@
+package cn.icods.jenkinsdemo;/**
+   * @ClassName JenkinsTest 
+   * @Description TODO
+   * @Author rex.zhao
+   * @Date 2022/8/15 3:56 PM
+   * @Version 1.0
+   */ public class JenkinsTest {
+}
